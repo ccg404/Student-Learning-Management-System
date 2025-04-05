75a6bc3d0cf4d7b67c38a55c6dba58375c76e514
ccg404 <galarritachristiancarlo3@gmail.com>
2025-04-05 22:53:27 +0800

    Refactor layout and styling for main content, featured courses, and announcements sections; enhance sidebar functionality

--- script/script.js
+++ script/script.js
@@ -3,12 +3,18 @@ const sidebarTitle = document.getElementsByTagName("h2")[0];
 
 const button = document.getElementById("collapseButton").onclick = () => {
     const sidebarContainer = document.getElementById("sidebar-container");
-    if (sidebarContainer.style.display === "none") {
-        sidebarContainer.style.display = "block";
+    if (sidebarContainer.style.display === "none" || sidebarContainer.style.width === "0px") {
+        sidebarContainer.style.display = "flex";
         sidebarContainer.style.transition = "width 0.5s ease-in-out";
+        sidebarContainer.style.width = "15%";
     } else {
+        sidebarContainer.style.transition = "width 0.5s ease-in-out";
         sidebarContainer.style.display = "block";
-        sidebarContainer.style.width = "110px";
-        sidebarTitle.innerHTML = "X";
+        sidebarContainer.style.width = "0px";
+        button.style.display= "block";
+        button.style.position = "absolute";
+        button.style.right = "100px";
+        button.style.top = "0px";
+        button.style.color = "black";
     }
 }
